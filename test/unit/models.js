79cfb47db9ce7622db8523e3d796499d326b3277
lucasam1992 <lucas.almeida.machado92@gmail.com>
2022-05-13 21:34:16 -0300

    testes no salesmodel

--- test/unit/models.js
+++ test/unit/models.js
@@ -7,7 +7,7 @@ const mongoConnection = require('../../models/connection');
 const salesModel = require('../../models/salesModel');
 const productsModel = require('../../models/productsModel');
 
-// Link que me ajudou a recaptular o Mock da conexão para fazer os testes
+// Recaptulando o Mock da conexão para fazer os testes
 // https://github.com/tryber/sd-10a-live-lectures/blob/lecture/28.3/nodejs-jwt-tests-base-project/tests/connectionMock.js
 
 let connectionMock;
@@ -19,13 +19,14 @@ before(async () => {
     URLMock,
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
-  connectionMock = connectionMock.db('StoreManager')
+  connectionMock = connectionMock.db('StoreManager');
   sinon.stub(mongoConnection, 'getConnection').resolves(connectionMock);
 });
 
 after(() => {
   mongoConnection.getConnection.restore();
 });
+
 
 const payloadProduct = {
   name: 'Produto Silva',
@@ -65,21 +66,94 @@ describe('Testa endpoints do produto', () => {
   
   describe('Testa se está atualizando os produtos', () => {
     it('retorna objeto atualizado', async () => {
-      const resultCreated = await productsModel.create(payloadProduct);
-      await productsModel.update(resultCreated._id, 'Produto Juracy', 25);
-      const resultProductById = await productsModel.getById(resultCreated._id);
+      const { _id: id } = await productsModel.create(payloadProduct);
+      const payloadProductUpdate = [{ 'name': 'Produto Juracy', 'quantity': 25 }];
+      
+      await productsModel.update(id, payloadProductUpdate);
+      
+      const { name, quantity } = await productsModel.getById(id);
 
-      expect(resultProductById.name).to.be.equal('Produto Juracy');
-      expect(resultProductById.quantity).to.be.equal(25);
+      expect(name).to.be.equal('Produto Juracy');
+      expect(quantity).to.be.equal(25);
     });
   });
   
   describe('Testa se está removendo os produtos', () => {
-    it('retorna vazio caso tenha sido removido da lista ', async () => {
-      const resultCreated = await productsModel.create(payloadProduct);
-      await productsModel.remove(resultCreated._id);
-      const resultProductById = await productsModel.getById(resultCreated._id);
+    it('retorna vazio caso tenha sido removido da lista', async () => {
+      const { _id: id} = await productsModel.create(payloadProduct);
+      await productsModel.remove(id);
+      const resultProductById = await productsModel.getById(id);
       
+      expect(resultProductById).to.be.equal(null);
+    });
+  });
+});
+
+
+// Validando Sale
+
+describe('Testa endpoints do sale', () => {
+  describe('Testa se está inserindo com sucesso', () => {
+    it('retorna um objeto', async () => {
+        const { _id: id } = await productsModel.create(payloadProduct);
+        const payloadSale = [{'productId': id,'quantity': 3}]
+        const result = await salesModel.create(payloadSale);
+        
+        expect(result).to.be.a('object');
+    });
+    it('verificar se existem os atributos _id, e itensSold', async () =>{
+        const { _id: id} = await productsModel.create(payloadProduct);
+        const payloadSale = [{'productId': id,'quantity': 3}];
+        const result = await salesModel.create(payloadSale);
+
+        expect(result).to.have.a.property('_id');
+        expect(result).to.have.a.property('itensSold');
+    });
+  });
+  describe('Testa se está pesquisando por id', () => {
+    it('retorna um objeto', async () => {
+      const { _id: id } = await productsModel.create(payloadProduct);
+      const payloadSale = [{'productId': id,'quantity': 3}];
+      const { _id: saleId } = await salesModel.create(payloadSale);
+      const result = await salesModel.getById(saleId);
+
+      expect(result).to.be.a('object');
+    });
+  });
+  describe('Testa se está pesquisando por todos os sales', () => {
+    it('retorna um array de objetos', async () => {
+      const result = await salesModel.getAll();
+
+      expect(result).to.be.a('array');
+    });
+  });
+  describe('Testa se está atualizando os sales', () => {
+    it('retorna um objeto atualizado', async () => {
+      // Criando product
+      const { _id: id} = await productsModel.create(payloadProduct);
+      // Criando Sale
+      const payloadSale = [{'productId': id,'quantity': 3}];
+      const { _id: saleId } = await salesModel.create(payloadSale);
+      // Fazendo alteração no sale
+      const payloadSaleUpdated = [{'productId': id, 'quantity': 25}];
+      const resultUpdated = await salesModel.update(saleId, payloadSaleUpdated);
+
+      const { quantity } = await salesModel.getById(saleId);
+      
+      expect(quantity).to.be.equal(25);
+    });
+  });
+  describe('Testa se está removendo os sales', () => {
+    it('retorna vazio caso tenha sido removido da lista', async () => {
+      // Criando product
+      const { _id: id} = await productsModel.create(payloadProduct);
+      // Criando Sale
+      const payloadSale = [{'productId': id,'quantity': 3}];
+      const { _id: saleId } = await salesModel.create(payloadSale);
+      await salesModel.remove(saleId);
+
+      const resultProductById = await salesModel.getById(saleId);
+
       expect(resultProductById).to.be.equal(null);
     });
   });
