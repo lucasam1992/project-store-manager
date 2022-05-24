9e25c4c15af71b535ca7e77659695478d0a6b086
lucasam1992 <lucas.almeida.machado92@gmail.com>
2022-05-24 15:02:12 -0300

    Correcao test controllerstatus

--- test/unit/controllers.js
+++ test/unit/controllers.js
@@ -128,7 +128,7 @@ describe('Testa enpoints do produto na camada controller', () => {
             it('é chamado o status com o código 422', async () => {
                 await productsController.getById(request, response, () => {});
 
-                expect(response.json.calledWith(messageError)).to.be.equal(true);
+                expect(response.status.calledWith(422)).to.be.equal(true);
             });
             it('é chamado json com a mensagem de erro genérica', async () => {
                 await productsController.getById(request, response, () => {});
@@ -196,7 +196,7 @@ describe('Testa enpoints do produto na camada controller', () => {
             it('é chamado o status com o código 422', async () => {
                 await productsController.update(request, response, () => {});
 
-                expect(response.json.calledWith(messageError)).to.be.equal(true);
+                expect(response.status.calledWith(422)).to.be.equal(true);
             });
             it('é chamado json com a mensagem de erro genérica', async () => {
                 await productsController.update(request, response, () => {});
@@ -226,7 +226,7 @@ describe('Testa enpoints do produto na camada controller', () => {
             it('é chamado o status com o código 200', async () => {
                 await productsController.update(request, response, () => {});
 
-                expect(response.json.calledWith(mockProducts)).to.be.equal(true);
+                expect(response.status.calledWith(200)).to.be.equal(true);
             });
             it('retorna um array', async () => {
                 await productsController.update(request, response, () => {});
@@ -258,7 +258,7 @@ describe('Testa enpoints do produto na camada controller', () => {
             it('é chamado status com o código 422', async () => {
                 await productsController.remove(request, response, () => {});
 
-                expect(response.json.calledWith(messageError)).to.be.equal(true);
+                expect(response.status.calledWith(422)).to.be.equal(true);
             });
             it('é chamado json com a mensagem de erro genérica', async () => {
                 await productsController.remove(request, response, () => {});
@@ -288,7 +288,7 @@ describe('Testa enpoints do produto na camada controller', () => {
             it('é chamado o status com o código 200', async () => {
                 await productsController.remove(request, response, () => {});
 
-                expect(response.json.calledWith(mockProducts)).to.be.equal(true);
+                expect(response.status.calledWith(200)).to.be.equal(true);
             });
             it('retorna objeto', async () => {
                 await productsController.remove(request, response, () => {});
@@ -301,7 +301,74 @@ describe('Testa enpoints do produto na camada controller', () => {
 
 
 // Testando sales
-
+/*
 describe('Testa enpoints da venda na camada controller', () => {
+    describe('Testa inserção de vendas', () => {
+        describe('quando payload informado não é válido', () => {
+            const response = {};
+            const request = {};
+            const itensSold = [{
+                productId: '604cb554311d68f491ba5781',
+                quantity: 15,
+            }];
+            const messageError = { err: { message: 'invalid_data' }};
 
+            before(() => {
+                request.body = { itensSold };
+
+                response.status = sinon.stub().returns(response);
+                response.json = sinon.stub().returns();
+
+                sinon.stub(productsService, 'create').resolves( messageError );
+            });
+
+            after(() => {
+                productsService.create.restore();
+            });
+
+            it('é chamado o status com o código 422', async () => {
+                await productsController.create(request, response, () => {});
+
+                expect(response.status.calledWith(422)).to.be.equal(true);
+                
+            });
+            it('é chamado json com a mensagem de erro genérica', async () => {
+                await productsController.create(request, response, () => {});
+
+                expect(response.json.calledWith(messageError)).to.be.equal(true);
+            });
+        });
+        describe('Quando payload informado é válido', () => {
+            const response = {};
+            const request = {};
+            const mockProducts = { products: [{name: 'Chapéu de Cobra', quantity: 15}] };
+
+            before(() => {
+                request.body = { mockProducts };
+
+                response.status = sinon.stub().returns(response);
+                response.json = sinon.stub().returns();
+
+                sinon.stub(productsService, 'create').resolves(mockProducts);
+            });
+
+            after(() => {
+                productsService.create.restore();
+            });
+
+            it('é chamado o status com o código 201', async () => {
+                await productsController.create(request, response, () => {});
+
+                expect(response.status.calledWith(201)).to.be.equal(true);
+            });
+
+            it('retorna objeto', async () => {
+                await productsController.create(request, response, () => {});
+
+                expect(response.json.calledWith(mockProducts)).to.be.equal(true);
+            }); 
+
+        });
+    });
 });
+*/
